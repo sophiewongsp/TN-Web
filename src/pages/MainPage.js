c467dc6538600a0329e8e4d26d1b5267415da1b1
Sophie <sophiewongsp@gmail.com>
2020-10-28 22:57:16 +0800

    ✨FEAT (SignUp): add form input elements

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const MainPage = () => {
   return (
     <div>
       <h1>Main page</h1>
+        <Link to="/signIn">
+          <button type="button">
+            Sign In
+          </button>
+        </Link>
     </div>
   );
 }
--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -4,6 +4,19 @@ const SignInPage = () => {
   return (
     <div>
       <h1>Sign In page</h1>
+      <form>
+        <label>
+          Username: 
+          <input type="text"/>
+        </label>
+        <br/>
+        <label>
+          Password: 
+          <input type="password"/>
+        </label>
+        <br/>
+        <input type="submit" value="Submit"/>
+      </form>
     </div>
   );
 }
